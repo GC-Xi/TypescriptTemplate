8412d2159c83e1a0f9cbd6d3cd8733d0278b139b
Xi <xwei@utexas.edu>
2020-06-19 10:15:44 -0700

    Typescript project setup

--- src/scripts/file.ts
+++ src/scripts/file.ts
@@ -2,3 +2,9 @@ function displayTotalPerPerson(person: string, total: number) {
     const message: string = "Total for " + person + " is " + total;
     document.getElementById("totalMessage").innerText = message;
 }
+
+function test() {
+    const map: { [key: string]: number } = {"one": 1, "two": 2};
+    const message: string = Object.keys(map).length.toString();
+    document.getElementById("totalMessage").innerText = message;
+}
